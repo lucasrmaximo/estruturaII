f36910fe6d3d5b724ea64f6748b06061b3249749
lucasrmaximo <lucasrmaximo@gmail.com>
2017-09-04 21:32:32 -0300

    Aula 04

---
+++ arvore.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _no {
+	int id;
+	struct _no *esq;
+	struct _no *dir;
+	struct _no *pai;
+} NO;
+
+=================
+===inicializa====
+=================
+
+void inicializa(NO **root){
+	*root = NULL;
+	
+}
+
+================
+====inserir=====
+================
+
+void inserir(NO **root, int id){
+	
+	
+}
+
+=================
+===funcao main===
+=================
+
+int main(
+	No *arvore;
+	inicializa(&arvore);
+	
+	inserir(&arvore);
+);
