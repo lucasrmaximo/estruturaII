f8a933b1d3c70b5b755f318d552426d80c067847
lucasrmaximo <lucasrmaximo@gmail.com>
2017-09-04 22:00:15 -0300

    AULA 04 pela metade

--- arvore.c
+++ arvore.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _no {
+typedef struct _no {            // estrutura principal
 	int id;
 	struct _no *esq;
 	struct _no *dir;
@@ -12,8 +12,8 @@ typedef struct _no {
 ===inicializa====
 =================
 
-void inicializa(NO **root){
-	*root = NULL;
+void inicializa(NO **root){             // a função ira setar null no ponteiro auxiliar
+	*root = NULL;                   // I am ROOT
 	
 }
 
@@ -22,8 +22,26 @@ void inicializa(NO **root){
 ================
 
 void inserir(NO **root, int id){
+	NO *novo;			// cria um novo nó
+	NO *aux;
+	NO *catra;     //variavel original(papai) eh muito gay, mas foi o sergio que colocou
+                            
+	novo = (NO *) malloc(sizeof(NO));  // reserva o espaço no sistema
 	
+	novo->id = num;
+	novo->pai = NULL;
+	novo->esq = NULL;
+	novo->dir = NULL;
 	
+	if(isEmpty(*root))		// verifica se se a arvore está vazia
+		*root = novo;		
+	
+	else {
+		aux = *root;
+		while (aux != NULL) {
+		
+		}
+	}
 }
 
 =================
